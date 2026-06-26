ac4557c1d37daf05f918fcc6d52cebe0a86627cd
mahinalshawon001 <mahinalshawon01@gmail.com>
2026-06-27 01:46:22 +0600

    Add files via upload

---
+++ Ballon burst.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ( )
+{
+ int balloons=4;
+ printf ("Before Burst:%d\n",balloons);
+ printf ("Burst ballon:%d\n",balloons--);
+ printf ("Remaining:%d\n",balloons);
+ return 0;
+}
